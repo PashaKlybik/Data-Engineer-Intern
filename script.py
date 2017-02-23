1a6929322650d6d82f225cd84c9cdf0b4966d6ec
PashaKlybik <pashaklybik@gmail.com>
2017-02-23 13:44:44 +0300

    ver-1

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import csv
 from bs4 import BeautifulSoup
 import urllib3
+import argparse
+
 
 
 def find_url(url):
@@ -14,17 +16,21 @@ def find_url(url):
 		if  i.find("th", text = "Website")!=None:
 			return i.find("a")["href"]
 		
-def main():	
-	wiki_urls = list()
-	with open("wikipedia_links.csv",'r') as read:
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("File")
+	args = parser.parse_args()
+	wiki_urls = [["wikipedia_page_url", "website_url"]]
+	with open(args.File,'r') as read:
 		reader = csv.reader(read)
 		for row in reader:
 			wiki_urls.append(row)
 
-	for id in range(len(wiki_urls)):
+	for id in range(1, len(wiki_urls)):
 		temp = find_url(wiki_urls[id][0])
 		wiki_urls[id].append(temp)
 
-	with open("wikipedia_answers_example1.csv", "w") as out_file:
+	with open("wikipedia_answers.csv", "w") as out_file:
 		writer = csv.writer(out_file)
 		writer.writerows(wiki_urls)
+ 
